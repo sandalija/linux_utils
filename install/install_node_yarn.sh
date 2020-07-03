7ebc8d154c7bb22c0bd05b46fbe3a97101fd603c
Sergio Candalija Valderrama <sergiocava.d@gmail.com>
2020-07-03 23:13:38 +0200

    Brave & node + yarn

---
+++ install/install_node_yarn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# NODE JS 12.x
+
+sudo apt update
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt -y install nodejs
+
+node --version
+
+# YARN 1.22.4
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
+YARNVERSION=$(yarn --version)
+
+export PATH="$PATH:/opt/yarn-${YARNVERSION}/bin"
